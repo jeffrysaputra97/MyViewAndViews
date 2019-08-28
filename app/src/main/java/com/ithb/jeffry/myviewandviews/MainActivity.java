5486868c2ec17c636092b1f8fc6ae0f9fa34edc8
Jeffry Saputra <jeffrysaputra97@yahoo.com>
2019-08-28 15:39:58 +0700

    Minor code update from Dicoding references

--- app/src/main/java/com/ithb/jeffry/myviewandviews/MainActivity.java
+++ app/src/main/java/com/ithb/jeffry/myviewandviews/MainActivity.java
@@ -1,15 +1,17 @@
 package com.ithb.jeffry.myviewandviews;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        getSupportActionBar().setTitle("Google Pixel");
+        if (getSupportActionBar() != null) {
+            getSupportActionBar().setTitle("Google Pixel");
+        }
     }
+
 }
